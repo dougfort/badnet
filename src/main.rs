a3871a5296db7abda08535b37eb055142f9b68ef
dougfort <doug.fort@gmail.com>
2021-12-24 08:20:55 -0500

    basic tokio stub

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::Error;
+
+#[tokio::main]
+async fn main() -> Result<(), Error> {
+    // Set the RUST_LOG, if it hasn't been explicitly defined
+    if std::env::var_os("RUST_LOG").is_none() {
+        std::env::set_var("RUST_LOG", "badnet=debug")
+    }
+    tracing_subscriber::fmt::init();
+    tracing::info!("program starts");
+
+    shutdown_signal().await;
+    Ok(())
+}
+
+/// unix signal handler
+/// pasted from Axum exampe
+async fn shutdown_signal() {
+    use std::io;
+    use tokio::signal::unix::SignalKind;
+
+    async fn terminate() -> io::Result<()> {
+        tokio::signal::unix::signal(SignalKind::terminate())?
+            .recv()
+            .await;
+        Ok(())
+    }
+
+    tokio::select! {
+        _ = terminate() => {},
+        _ = tokio::signal::ctrl_c() => {},
+    }
+    tracing::info!("signal received, starting graceful shutdown")
 }
